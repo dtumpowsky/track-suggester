ad159c5d9c8ea2baa8f0b395793e2c1a31ca7529
CJ Rhone and David Tumpowsky <cj@>
2018-03-19 11:26:02 -0700

    Initial setup

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var doMath = function(number1, number2){
+	return number1 + number2 - number3;
+}
+
+var number1 = parseInt(prompt("Enter number"));
+var number2 = parseInt(prompt("Enter another number"));
+var number3 = parseInt(prompt("Enter number to subtract"));
+
+alert(doMath(number1, number2, number3));

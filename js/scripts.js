26ca33d6b0e51ee931529d72e96c60cf8a555312
CJ Rhone and David Tumpowsky <cj@>
2018-03-19 13:32:09 -0700

    add a bmi and temperature converter

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,35 @@
-var doMath = function(number1, number2){
-	return number1 + number2 - number3;
+var add = function(number1, number2){
+	return number1 + number2;
+}
+
+var subtract = function(number1, number2){
+	return number1 - number2;
+}
+
+var multiply = function(number1, number2){
+	return number1 * number2;
+}
+
+var divide = function(number1, number2){
+	return number1 / number2;
 }
 
 var number1 = parseInt(prompt("Enter number"));
 var number2 = parseInt(prompt("Enter another number"));
-var number3 = parseInt(prompt("Enter number to subtract"));
 
-alert(doMath(number1, number2, number3));
+alert(add(number1, number2));
+
+
+//BMI Calculator
+
+var bmi = function(height, weight){
+	return (weight / (height^2));
+}
+
+var height = prompt("How tall are you? (in)");
+var weight = prompt("How much do you weigh?");
+
+var kg = weight * 2.2;
+var cm = height * 2.54;
+
+alert(bmi(cm, kg));

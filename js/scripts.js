029394cae991e78366399e7065dc7d84debc0b9a
David Tumpowsky <tumpowskydavid@gmail.com>
2018-03-23 13:40:32 -0700

    include preliminary back end logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,32 @@
+
+//Back-end Logic//
+
+
+var trackCSS = "CSS"
+var trackPHP = "PHP"
+var trackC# = "C#"
+var trackJava = "Java"
+
+var interest = $("#csInterest").val();
+
+var city = $("#city").val();
+
+var workEnviro = $("#wkEnviro").val();
+
+var corpSeattle = $("#corpSeattle").val();
+
+var corpSanFran = $("#corpSanFran").val()
+
+
+
+
+if (city === #seattle && )
+
+
+
+
+//Front-end Logic//
+
 $(document).ready(function() {
 
 

3669edc6f36d4cdbbafbdb7daf5e85b70b40c35f
David Tumpowsky <tumpowskydavid@gmail.com>
2018-03-23 16:16:25 -0700

    experiment with submit vs click button functionality with the website

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@
 
 var trackCSS = "CSS"
 var trackPHP = "PHP"
-var trackC# = "C#"
+var trackSharp = "C#"
 var trackJava = "Java"
 
 var interest = $("#csInterest").val();
@@ -14,24 +14,24 @@ var corpSeattle = $("#corpSeattle").val();
 var corpSanFran = $("#corpSanFran").val()
 
 
-if (city === "#seattle" || "sanFran" && interest === "#frontEnd") {
-	return trackCSS
+if (city === "#seattle" || "#sanFran" && interest === "#frontEnd") {
+	alert(trackCSS);
 }
 
-if (city === "#seattle" && interest === "#backEnd" && workEnviro === "corp" && corpSeatte === "corpAmazon") {
-	return trackJava
+if (city === "#seattle" && interest === "#backEnd" && workEnviro === "#corp" && corpSeatte === "corpAmazon") {
+	alert(trackJava);
 } else {
-	return trackC#
+	alert(trackSharp);
 }
 
-if (city === "#sanFran" && interest === "#backEnd" && workEnviro === "corp" && corpSanFran === "corpGoogle") {
-	return trackJava
+if (city === "#sanFran" && interest === "#backEnd" && workEnviro === "#corp" && corpSanFran === "corpGoogle") {
+	alert(trackJava);
 } else {
-	return trackC#
+	alert(trackSharp);
 }
 
-if (city === "#seattle" || "sanFran" && interest === "#backEnd" & workEnviro === "#startUp") {
-	return trackPHP
+if (city === "#seattle" || "#sanFran" && interest === "#backEnd" & workEnviro === "#startUp") {
+	alert(trackPHP);
 }
 
 
@@ -48,8 +48,37 @@ $(document).ready(function() {
 		var response = (confirm("Welcome to the next chapter " + name));
 
 		if (response == true) {
-			$("#csInterest").show();
+			$("#city").show();
+			$("button#next2").show();
 		}
 	});
+
+	$("#city form").submit(function(event) {
+		var citySelect = $("input:radio[name=question2]:checked").val();
+		event.preventDefault();
+
+		$("button#next2").submit(function() {
+			if (citySelect === "#seattle" || "#sanFran") {
+			$("#interest").show();
+			$("button#next3").show();
+		}
+		});
+
+	});
+
+	$("button#next3").click(function() {
+		var interestSelect = $("input:radio[name=question3]:checked").val();
+		$("button#next4").show();
+
+	/*	if (interestSelect === "#backEnd") {
+			$("workEnviro").show();
+		} else {
+			$(alert("CSS is the best track for you!"));
+		}*/
+
+
+	});
+
+
 
 });

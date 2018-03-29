8fea457ed22e460ed09ecd50537c3809cdddc8fe
David Tumpowsky <tumpowskydavid@gmail.com>
2018-03-28 22:23:06 -0700

    rework logic to be more clear

--- js/scripts.js
+++ js/scripts.js
@@ -1,101 +1,175 @@
 
-//Back-end Logic//
-
-
-var trackCSS = "CSS"
-var trackPHP = "PHP"
-var trackSharp = "C#"
-var trackJava = "Java"
-
-var city = $("input:radio[name=question2]:checked").val();
-var interest = $("input:radio[name=question3]:checked").val();
-var workEnviro = $("input:radio[name=question4]:checked").val();
-var corpSeattle = $("input:radio[name=question5]:checked").val();
-var corpSanFran = $("input:radio[name=question5-2]:checked").val();
-
-
-if (interest == "frontEnd") {
-	return (trackCSS);
-}
-
-	if (city == "#seattle") {
-		if (interest == "#backEnd") {
-		 	if (workEnviro == "#corp") {
-				if (corpSeatte == "corpAmazon") {
-					return(trackJava);
-				} else {
-					return(trackSharp);
-				}
-			}
-		}
-	}
-
-	if (city == "#sanFran") {
-		if (interest == "#backEnd") {
-			if (workEnviro == "#corp") {
-				if (corpSanFran == "corpGoogle") {
-					return(trackJava);
-				} else {
-				  return(trackSharp);
-				}
-			}
-		}
-	}
-
-
-
-if (city == "#seattle" || "#sanFran") {
-	if (interest == "#backEnd")
-		if (workEnviro == "#startUp") {
-	return(trackPHP);
-		} else {
-	}
-}
-
-
-
-
-
-//Front-end Logic//
-
+// Front End
 $(document).ready(function() {
+	$("form#trackSelect").submit(function(event) {
+ 		event.preventDefault();
 
-	$("button#next1").click(function() {
 		var name = $("input#nameInput").val();
-		var response = (confirm("Welcome to the next chapter " + name));
+		var response = alert("Welcome to the next chapter " + name);
 
-		if (response == true) {
-			$("#city").show();
+		var city = $("input:radio[name=question2]:checked").val();
+		var path = $("input:radio[name=question3]:checked").val();
+		var workEnviro = $("input:radio[name=question4]:checked").val();
+		var corpSeattle = $("input:radio[name=question5]:checked").val();
+		var corpSanFran = $("input:radio[name=question5-2]:checked").val();
+
+		$("button#next1").submit(function() {
+			console.log(hello)
+			$("#start").show();
 			$("button#next2").show();
-		}
-	});
-
-	$("#city form").submit(function(event) {
-		var citySelect = $("input:radio[name=question2]:checked").val();
-
-		$("button#next2").submit(function() {
-			if (citySelect == "#seattle" || "#sanFran") {
-			$("#interest").show();
-			$("button#next3").show();
-		}
-		event.preventDefault();
 		});
 
-	});
-
-	$("button#next3").click(function() {
-		var interestSelect = $("input:radio[name=question3]:checked").val();
-		$("button#next4").show();
-
-	/*	if (interestSelect == "#backEnd") {
-			$("workEnviro").show();
-		} else {
-			$(alert("CSS is the best track for you!"));
-		}*/
-
-
-	});
-
-
-
+  });
 });
+
+
+
+
+
+
+
+
+// //Back-end Logic//
+//
+//
+// var track1 = "CSS"
+// var track2 = "PHP"
+// var track3 = "C#"
+// var track4 = "Java"
+//
+// // var city = $("input:radio[name=question2]:checked").val();
+// // var path = $("input:radio[name=question3]:checked").val();
+// // var workEnviro = $("input:radio[name=question4]:checked").val();
+// // var corpSeattle = $("input:radio[name=question5]:checked").val();
+// // var corpSanFran = $("input:radio[name=question5-2]:checked").val();
+//
+//
+// // if (path == "#frontEnd") {
+// // 	("#result") = track1
+// // }
+// //
+// // if (city == "#seattle") && (path == "#backEnd") && (workEnviro == "#corp") && (corpSeatte == "#corpAmazon") {
+// // 	("#result" = track4);
+// // } else {
+// // 	("#result" = track3);
+// // };
+// //
+// // if (city == "#sanFran") && (path == "#backEnd") && if (workEnviro == "#corp") && if (corpSanFran == "#corpGoogle") {
+// // 	("#result" = track4);
+// // } else {
+// // 	("#result" = track3);
+// // };
+// //
+// //
+// //
+// // if (city == "#seattle" || "#sanFran") && if (path == "#backEnd") && {if (workEnviro == "#startUp") {
+// // 	("#result" = track2);
+// // };
+//
+//
+//
+//
+//
+// //Front-end Logic//
+//
+// $(document).ready(function() {
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// 	// $("button#next1").click(function() {
+// 	// 	event.preventDefault();
+// 	// 	var name = $("input#nameInput").val();
+// 	// 	var response = (confirm("Welcome to the next chapter " + name));
+// 	//
+// 	// 	if (response == true) {
+// 	// 		$("#city").show();
+// 	// 		$("button#next2").show();
+// 	// 	}
+// 	// });
+// 	//
+// 	// var city = $("input:radio[name=question2]:checked").val();
+// 	// var path = $("input:radio[name=question3]:checked").val();
+// 	// var workEnviro = $("input:radio[name=question4]:checked").val();
+// 	// var corpSeattle = $("input:radio[name=question5]:checked").val();
+// 	// var corpSanFran = $("input:radio[name=question5-2]:checked").val();
+// 	//
+// 	//
+// 	// if (path == "#frontEnd") {
+// 	// 	"#result" = track1
+// 	// } else {
+// 	//
+// 	// };
+// 	//
+// 	// if (city == "#seattle") && (path == "#backEnd") && (workEnviro == "#corp") && (corpSeatte == "#corpAmazon") {
+// 	// 	("#result" = track4);
+// 	// } else {
+// 	// 	("#result" = track3);
+// 	// };
+// 	//
+// 	// if (city == "#sanFran") && (path == "#backEnd") && if (workEnviro == "#corp") && if (corpSanFran == "#corpGoogle") {
+// 	// 	("#result" = track4);
+// 	// } else {
+// 	// 	("#result" = track3);
+// 	// };
+// 	//
+// 	//
+// 	//
+// 	// if (city == "#seattle" || "#sanFran") && if (path == "#backEnd") && {if (workEnviro == "#startUp") {
+// 	// 	("#result" = track2);
+// 	// };
+// 	//
+// 	// // $("#city form").submit(function(event) {
+// 	// // 	event.preventDefault();
+// 	// // 	var citySelect = $("input:radio[name=question2]:checked").val();
+// 	// //
+// 	// // 	$("button#next2").submit(function() {
+// 	// // 		if (citySelect == "#seattle" || "#sanFran") {
+// 	// // 		$("#interest").show();
+// 	// // 		$("button#next3").show();
+// 	// // 	}
+// 	// // 	});
+// 	// //
+// 	// // });
+// 	// //
+// 	// // $("button#next3").click(function() {
+// 	// // 	var interestSelect = $("input:radio[name=question3]:checked").val();
+// 	// 	$("button#next4").show();
+//
+// 	/*	if (interestSelect == "#backEnd") {
+// 			$("workEnviro").show();
+// 		} else {
+// 			$(alert("CSS is the best track for you!"));
+// 		}*/
+//
+//
+// 	});
+//
+//
+//
+// });

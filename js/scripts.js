0c3ba8ab7c10eb8d51c3a1dc9b5d3ce9748e3209
David Tumpowsky <tumpowskydavid@gmail.com>
2018-03-29 12:53:41 -0700

    include reworked html

--- js/scripts.js
+++ js/scripts.js
@@ -7,17 +7,38 @@ $(document).ready(function() {
 		var name = $("input#nameInput").val();
 		var response = alert("Welcome to the next chapter " + name);
 
+		var track1 = "CSS"
+		var track2 = "PHP"
+		var track3 = "C#"
+		var track4 = "Java"
+//Radio Inputs
 		var city = $("input:radio[name=question2]:checked").val();
 		var path = $("input:radio[name=question3]:checked").val();
 		var workEnviro = $("input:radio[name=question4]:checked").val();
 		var corpSeattle = $("input:radio[name=question5]:checked").val();
 		var corpSanFran = $("input:radio[name=question5-2]:checked").val();
 
-		$("button#next1").submit(function() {
-			console.log(hello)
-			$("#start").show();
-			$("button#next2").show();
-		});
+		if ((city == "#seattle") && (path == "#backEnd") && (workEnviro == "#corp") && (corpSeatte == "#corpAmazon")) {
+		 	alert(track4);
+			} else {
+			alert(track3);
+		 };
+
+		 if ("#result" == track4) {
+			 alert("Java")
+		 }
+
+		 if ("#result" == track3) {
+			 alert("C#")
+		 }
+
+		 if ("#result" == track2) {
+			 alert("PHP")
+		 }
+
+		 if ("#result" == track2) {
+			 alert("PHP")
+		 }
 
   });
 });

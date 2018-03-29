d3f2363ba9805f250b3204414f98d35b1e66b067
David Tumpowsky <tumpowskydavid@gmail.com>
2018-03-29 13:07:04 -0700

    add functionality progress

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,39 @@
+var track1 = "CSS"
+var track2 = "PHP"
+var track3 = "C#"
+var track4 = "Java"
+//Radio Inputs
+var city = $("input:radio[name=question2]:checked").val();
+var path = $("input:radio[name=question3]:checked").val();
+var workEnviro = $("input:radio[name=question4]:checked").val();
+var corpSeattle = $("input:radio[name=question5]:checked").val();
+var corpSanFran = $("input:radio[name=question5-2]:checked").val();
+
+
+if ((city == "#seattle") && (path == "#backEnd") && (workEnviro == "#corp") && (corpSeatte == "#corpAmazon")) {
+	alert(track4);
+} else if (corpSeattle == "#corpMicro") {
+	alert(track3);
+ };
+
+ if ("#result" == track4) {
+	 alert("Java")
+ }
+
+ if ("#result" == track3) {
+	 alert("C#")
+ }
+
+ if ("#result" == track2) {
+	 alert("PHP")
+ }
+
+ if ("#result" == track2) {
+	 alert("PHP")
+ }
+
+
+
 
 // Front End
 $(document).ready(function() {
@@ -7,40 +43,17 @@ $(document).ready(function() {
 		var name = $("input#nameInput").val();
 		var response = alert("Welcome to the next chapter " + name);
 
-		var track1 = "CSS"
-		var track2 = "PHP"
-		var track3 = "C#"
-		var track4 = "Java"
-//Radio Inputs
-		var city = $("input:radio[name=question2]:checked").val();
-		var path = $("input:radio[name=question3]:checked").val();
-		var workEnviro = $("input:radio[name=question4]:checked").val();
-		var corpSeattle = $("input:radio[name=question5]:checked").val();
-		var corpSanFran = $("input:radio[name=question5-2]:checked").val();
+		if (response == true) {
+			$("button#next1").submit(function() {
+    	$("form#city").show();
+		});
+	}
 
-		if ((city == "#seattle") && (path == "#backEnd") && (workEnviro == "#corp") && (corpSeatte == "#corpAmazon")) {
-		 	alert(track4);
-			} else {
-			alert(track3);
-		 };
 
-		 if ("#result" == track4) {
-			 alert("Java")
-		 }
 
-		 if ("#result" == track3) {
-			 alert("C#")
-		 }
 
-		 if ("#result" == track2) {
-			 alert("PHP")
-		 }
 
-		 if ("#result" == track2) {
-			 alert("PHP")
-		 }
-
-  });
+	});
 });
 
 

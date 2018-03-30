dc0db4a12543c9bdd7ef9187bf9380c867af2392
David Tumpowsky <tumpowskydavid@gmail.com>
2018-03-30 10:57:15 -0700

    continue rework of functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,63 @@
+var track1 = "CSS"
+var track2 = "PHP"
+var track3 = "Java"
+var track4 = "C#"
+var city;
+var path;
+var workEnviro;
+var corpSeattle;
+var corpSanFran;
+
 $(document).ready(function() {
-	$("form#trackSelect").submit(function(event) {
-	event.preventDefault();
+
+	$("button#next1").click(function() {
+		event.preventDefault();
+		var name = $("input#nameInput").val();
+		  alert("Welcome to the next chapter " + name + "!")
+		});
+
+
 
 	function getValues() {
-		var city = $("input:radio[name=question2]:checked").val();
-		var path = $("input:radio[name=question3]:checked").val();
-		var workEnviro = $("input:radio[name=question4]:checked").val();
-		var corpSeattle = $("input:radio[name=question5]:checked").val();
-		var corpSanFran = $("input:radio[name=question5-2]:checked").val();
+			city = $("input:radio[name=question2]:checked").val();
+		 	path = $("input:radio[name=question3]:checked").val();
+		 	workEnviro = $("input:radio[name=question4]:checked").val();
+		 	corpSeattle = $("input:radio[name=question5]:checked").val();
+		 	corpSanFran = $("input:radio[name=question5-2]:checked").val();
+	}
 
 
-    	getValues();
-    	event.preventDefault();
-			if ((city == "#seattle") && (path == "#backEnd") && (workEnviro == "#corp") && (corpSeatte == "#corpAmazon")) {
-			  alert(track4);
+		$("form#trackSelect").submit(function(event) {
+			getValues();
+		event.preventDefault();
+
+
+
+
+			if (city == "seattle" && path == "backEnd" && workEnviro == "corp" && corpSeatte == "corpAmazon") {
+			  alert("Java");
 			} else if (corpSeattle == "#corpMicro") {
-				alert(track3);
+				alert("C#");
 		  };
 
 			if ("#result" == track4) {
-				 alert("Java")
-			 }
+				 alert("Java");
+			}
 
-			 if ("#result" == track3) {
-				 alert("C#")
-			 }
+			if ("#result" == track3) {
+				 alert("C#");
+			}
 
-			 if ("#result" == track2) {
-				 alert("PHP")
-			 }
+			if ("#result" == track2) {
+				 alert("PHP");
+			}
 
-			 if ("#result" == track2) {
-				 alert("PHP")
-			 }
+			if ("#result" == track2) {
+				 alert("PHP");
+			}
 
 
-  });
+
+
+	})
 });
